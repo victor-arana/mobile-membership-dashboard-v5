6c580515194ff9dd2dd9f7791289779644b07d66
victor-arana <victor_arana@hotmail.com>
2020-09-12 01:16:28 -0500

    :construction: property-binding, pass data to the child component
    Changes committed:
    	modified:   src/app/app.component.html
    	modified:   src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TransactionDistributionService } from './services/transaction-distribution.service';
+import { TransactionDistribution } from './model/transaction-distribution';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'mobile-membership-dashboard-v5';
 
-  constructor(private service: TransactionDistributionService){}
+  public transactions: TransactionDistribution[];
+
+  constructor(private service: TransactionDistributionService){
+  }
+
+  ngOnInit(): void {
+    this.service.getTransactionDistribution(0,new Date(), new Date()).subscribe(d => this.transactions = d);
+  }
 }
