2792326f3df28cadfb6ea1cb20c178cddd519771
victor-arana <victor_arana@hotmail.com>
2020-09-12 00:12:39 -0500

    :construction: datatable, make the distributions property observable.
    Changes committed:
    	modified:   src/app/dashboard/material-table/material-table.component.ts

--- src/app/dashboard/material-table/material-table.component.ts
+++ src/app/dashboard/material-table/material-table.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TransactionDistribution } from 'src/app/model/transaction-distribution';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-material-table',
@@ -10,7 +11,7 @@ export class MaterialTableComponent implements OnInit {
 
   columnsToDisplay = ['level', 'regularTransactions', 'mobileTransactions'];
 
-  public distributions: TransactionDistribution[]
+  public distributions: Observable<TransactionDistribution[]>;
 
   constructor() { }
 
