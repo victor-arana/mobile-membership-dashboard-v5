d672969fe37be30c0640668cc24d339fb4e61e47
victor.arana <victor.arana@walmart.com>
2020-09-13 16:05:34 -0500

    :wrench: select-component, add logic to reusable select component
    Changes to be committed:
    	modified:   src/app/dashboard/select/select.component.html
    	modified:   src/app/dashboard/select/select.component.ts

--- src/app/dashboard/select/select.component.ts
+++ src/app/dashboard/select/select.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Input, Output } from '@angular/core';
+import { TransactionDistribution } from 'src/app/model/transaction-distribution';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.css']
 })
-export class SelectComponent implements OnInit {
+export class SelectComponent {
 
-  constructor() { }
+  @Input() public distributions: Observable<TransactionDistribution[]>;
+  @Output() public itemSelect: EventEmitter<TransactionDistribution>;
 
-  ngOnInit(): void {
+  constructor() { 
+    this.itemSelect = new EventEmitter<TransactionDistribution>();
   }
-
+  
+  onItemSelect(row){
+    console.log(row);
+    this.itemSelect.emit(row);
+  }
 }
